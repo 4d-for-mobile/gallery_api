65cabcfb32eb4818776f2efbbd9d444e947bbb37
Eric Marchand <eric.marchand@4d.com>
2020-01-22 16:27:32 +0100

    🎉 Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const https = require('https');
+
+exports.topics = function() {
+    return ["4d-for-ios-form-list", "4d-for-ios-form-detail","4d-for-ios-formatter", "4d-for-ios-form-login"];
+}
+
+exports.fetch = function(topic) {
+    https.get("https://raw.githubusercontent.com/4d-for-ios/gallery/master/"+topic+".txt", (resp) => {
+        let data = '';
+      
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+      
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          console.log(JSON.parse(data).explanation);
+        });
+      
+      }).on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+}
