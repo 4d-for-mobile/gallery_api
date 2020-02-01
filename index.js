8e0509486bfd52c2dc3f7275480318612f20ce08
mesopelagique <eric.marchand@4d.com>
2020-02-01 13:14:15 +0100

    Use promise and new urls with json data

--- index.js
+++ index.js
@@ -1,28 +1,85 @@
-const https = require('https');
+"use strict";
 
-const topics = ["4d-for-ios-form-list", "4d-for-ios-form-detail","4d-for-ios-formatter", "4d-for-ios-form-login"];
+const https = require("https");
+
+const topics = [
+  "4d-for-ios-form-list",
+  "4d-for-ios-form-detail",
+  "4d-for-ios-formatter",
+  "4d-for-ios-form-login"
+];
+const baseURL =
+  "https://raw.githubusercontent.com/4d-for-ios/gallery/master/Output/";
 
 // Return list of available topics
 exports.topics = function() {
-    return topics;
-}
+  return topics;
+};
 
-exports.fetch = function(topic, complete) {
-    https.get("https://raw.githubusercontent.com/4d-for-ios/gallery/master/"+topic+".txt", (resp) => {
-        let data = '';
-      
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
+// Fetch repositories information for one topic
+exports.repositories = function(topic) {
+  return new Promise((resolve, reject) => {
+    var url = baseURL + topic + "/index.json";
+    //console.log(url)
+    https
+      .get(url, (response) => {
+        var data = "";
+        response.on("data", (chunk) => {
           data += chunk;
         });
-      
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-          complete(data.split("\n"))
+        response.on("end", () => {
+          //console.log(data);
+          //console.log(response.statusCode );
+          if (response.statusCode > 200 || response.statusCode < 299) {
+            try {
+              var json = JSON.parse(data);
+              //console.log(json);
+              resolve(json);
+            } catch (e) {
+              //console.log(e);
+              reject(e);
+            }
+          } else {
+            reject(response);
+          }
         });
-      
-      }).on("error", (err) => {
-        console.log("Error: " + err.message);
-        complete([])
+      })
+      .on("error", err => {
+        reject(err);
       });
-}
+  });
+};
+
+// Fetch repositories information for one topic
+exports.repository = function(topic, project) {
+  return new Promise((resolve, reject) => {
+    let url = baseURL + topic + "/" + project + "/info.json";
+    //console.log(url);
+    https
+      .get(url, response => {
+        let data = "";
+        response.on("data", chunk => {
+          data += chunk;
+        });
+        response.on("end", () => {
+          //console.log(data);
+          //console.log(response.statusCode );
+          if (response.statusCode > 200 || response.statusCode < 299) {
+            try {
+              let json = JSON.parse(data);
+              //console.log(json);
+              resolve(json);
+            } catch (e) {
+              //console.log(e);
+              reject(e);
+            }
+          } else {
+            reject(response);
+          }
+        });
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+};
--- index.test.js
+++ index.test.js
@@ -12,11 +12,27 @@ describe("topics", function(){
 describe("repositories", function(){
     let topics = gallery_api.topics()
     topics.forEach((topic, index) => {
-        it("should return repo for topic "+topic, function(done){
-            gallery_api.fetch(topic, (repos) => {
-                expect(repos.length).to.be.above(0);
+        it("should return repos for topic "+topic, function(done){
+            gallery_api.repositories(topic)
+            .then(function(repos) {
+                expect(repos.items.length).to.be.above(0);
                 done();
+            }, function(error) {
+                console.log(error);
             });
+        });
+    });
+});
+
+describe("repository", function(){
+    let topic = "4d-for-ios-form-detail"
+    it("should return repo for topic "+topic, function(done){
+        gallery_api.repository(topic, "4d-for-ios/form-detail-Cards")
+        .then(function(repo) {
+            expect(repo.name).to.be.not.empty;
+            done();
+        }, function(error) {
+            console.log(error);
         });
     });
 });
