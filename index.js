e2c9f550b3b4a5371072db50d3d5f740c38c020f
eric.marchand <eric.marchand@4d.com>
2021-03-16 15:48:29 +0100

    Use node-fetch

--- index.js
+++ index.js
@@ -1,88 +1,32 @@
 "use strict";
 
-const https = require("https");
+const fetch = require('node-fetch');
 
 const topics = [
-  "4d-for-ios-form-list",
-  "4d-for-ios-form-detail",
-  "4d-for-ios-formatter",
-  "4d-for-ios-form-login"
+    "4d-for-ios-form-list",
+    "4d-for-ios-form-detail",
+    "4d-for-ios-formatter",
+    "4d-for-ios-form-login"
 ];
 
 const baseURL =
-  "https://4d-for-mobile.github.io/gallery/Specs/"; // "https://raw.githubusercontent.com/4d-for-ios/gallery/master/Specs/";
+    "https://4d-for-mobile.github.io/gallery/Specs/"; // "https://raw.githubusercontent.com/4d-for-ios/gallery/master/Specs/";
 
 exports.baseURL = baseURL
 
 // Return list of available topics
 exports.topics = function() {
-  return topics;
+    return topics;
 };
 
 // Fetch repositories information for one topic
 exports.repositories = function(topic) {
-  return new Promise((resolve, reject) => {
-    var url = this.baseURL + topic + "/index.json";
-    //console.log(url)
-    https
-      .get(url, (response) => {
-        var data = "";
-        response.on("data", (chunk) => {
-          data += chunk;
-        });
-        response.on("end", () => {
-          //console.log(data);
-          //console.log(response.statusCode );
-          if (response.statusCode > 200 || response.statusCode < 299) {
-            try {
-              var json = JSON.parse(data);
-              //console.log(json);
-              resolve(json);
-            } catch (e) {
-              //console.log(e);
-              reject(e);
-            }
-          } else {
-            reject(response);
-          }
-        });
-      })
-      .on("error", err => {
-        reject(err);
-      });
-  });
+    return fetch(this.baseURL + topic + "/index.json")
+        .then(res => res.json())
 };
 
 // Fetch repositories information for one topic
 exports.repository = function(topic, project) {
-  return new Promise((resolve, reject) => {
-    let url = this.baseURL + topic + "/" + project + "/info.json";
-    //console.log(url);
-    https
-      .get(url, response => {
-        let data = "";
-        response.on("data", chunk => {
-          data += chunk;
-        });
-        response.on("end", () => {
-          //console.log(data);
-          //console.log(response.statusCode );
-          if (response.statusCode > 200 || response.statusCode < 299) {
-            try {
-              let json = JSON.parse(data);
-              //console.log(json);
-              resolve(json);
-            } catch (e) {
-              //console.log(e);
-              reject(e);
-            }
-          } else {
-            reject(response);
-          }
-        });
-      })
-      .on("error", err => {
-        reject(err);
-      });
-  });
+    return fetch(this.baseURL + topic + "/" + project + "/info.json")
+        .then(res => res.json())
 };
